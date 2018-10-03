eef9b7ccbb6859d31d17bfcb76319289bb39baf9
小林和希 <kobayashi.kazuki74@gmail.com>
2018-10-03 12:44:24 +0900

    implemented user name login for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,56 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#  failed_attempts        :integer          default(0), not null
+#  unlock_token           :string
+#  locked_at              :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  name                   :string
+#
+
 class User < ApplicationRecord
+  attr_accessor :login
   # Include default devise modules. Others available are:
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable
+  validates :name, presence: true, uniqueness: { case_sensitive: false}
+
+  validates_format_of :name, with: /^[a-zA-Z0-9_¥]*$/, multiline: true
+  validate :validate_name
+
+   def login=(login)
+   	@login = login
+   end
+
+   def login
+   	@login || self.name || self.email
+   end
+
+   def validate_name
+   	errors.add(:name, :invalid) if User.where(email: name).exists?
+   end
+
+   def self.find_for_database_authentication(warden_conditions)
+   	conditions = warden_conditions.dup
+   	conditions[:email].downcase! if conditions[:email]
+   	login = conditions.delete(:login)
+
+   	where(conditions.to_hash).where(
+   		['lower(name) = :value OR lower(email) = :value',
+   			{ value: login.downcase }]
+   	).first
+   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
     adress: 'smtp.gmail.com',
     port: 587,
     authentication: :plain,
-    user_name: your_email@gmail.com,
-    password: your_password
+    user_name: "your_email@gmail.com",
+    password: "your_password"
   }
 end
